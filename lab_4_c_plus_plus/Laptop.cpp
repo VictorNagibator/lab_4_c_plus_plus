9c8dca4a556e9addb80db678e8c081260dccf586
Victor <vityadonskoy2004@mail.ru>
2023-11-11 17:15:50 +0700

    Исправил кодировку #3

--- lab_4_c_plus_plus/Laptop.cpp
+++ lab_4_c_plus_plus/Laptop.cpp
@@ -1,124 +1,123 @@
-Victor, [11/11/2023 17:07]
 #include "Laptop.h"
 
 void Laptop::operator=(Laptop other) {
-  this->modelName = other.getModelName();
-  this->cpu = other.getCPU();
-  this->gpu = other.getGPU();
-  this->motherboard = other.getMotherboard();
-  this->ram = other.getRAM();
-  this->display = other.getDisplay();
-  this->dataStorage = other.getDataStorage();
+    this->modelName = other.getModelName();
+    this->cpu = other.getCPU();
+    this->gpu = other.getGPU();
+    this->motherboard = other.getMotherboard();
+    this->ram = other.getRAM();
+    this->display = other.getDisplay();
+    this->dataStorage = other.getDataStorage();
 }
 
 std::ostream& operator << (std::ostream& out, const Laptop& laptop) {
-  out << laptop.toString();
-  return out;
+    out << laptop.toString();
+    return out;
 }
 
 Laptop::Laptop(std::string modelName, CPU cpu, GPU gpu, RAM ram, Motherboard motherboard, Display display, DataStorage* dataStorage) {
-  tryToSetArguments(modelName, cpu, gpu, ram, motherboard, display, dataStorage);
+    tryToSetArguments(modelName, cpu, gpu, ram, motherboard, display, dataStorage);
 }
 
 std::string Laptop::getModelName() const {
-  return modelName;
+    return modelName;
 }
 
 const CPU& Laptop::getCPU() const {
-  return cpu;
+    return cpu;
 }
 
 const GPU& Laptop::getGPU() const {
-  return gpu;
+    return gpu;
 }
 
 const RAM& Laptop::getRAM() const {
-  return ram;
+    return ram;
 }
 
 const Motherboard& Laptop::getMotherboard() const {
-  return motherboard;
+    return motherboard;
 }
 
 const Display& Laptop::getDisplay() const {
-  return display;
+    return display;
 }
 
 DataStorage* Laptop::getDataStorage() const {
-  return dataStorage;
+    return dataStorage;
 }
 
 void Laptop::setModelName(std::string modelName) {
-  this->modelName = modelName;
+    this->modelName = modelName;
 }
 
 void Laptop::setCPU(CPU cpu) {
-  if (checkArguments(this->modelName, cpu, this->gpu, this->ram, this->motherboard, this->display, this->dataStorage)) {
-    this->cpu = cpu;
-  }
-  else throw std::invalid_argument("Неподходящий сокет!");
+    if (checkArguments(this->modelName, cpu, this->gpu, this->ram, this->motherboard, this->display, this->dataStorage)) {
+        this->cpu = cpu;
+    }
+    else throw std::invalid_argument("Неподходящий сокет!");
 }
 
 void Laptop::setGPU(GPU gpu) {
-  this->gpu = gpu;
+    this->gpu = gpu;
 }
 
 void Laptop::setRAM(RAM ram) {
-  if (checkArguments(this->modelName, this->cpu, this->gpu, ram, this->motherboard, this->display, this->dataStorage)) {
-    this->ram = ram;
-  }
-  else throw std::invalid_argument("Неподходящий тип памяти!");
+    if (checkArguments(this->modelName, this->cpu, this->gpu, ram, this->motherboard, this->display, this->dataStorage)) {
+        this->ram = ram;
+    }
+    else throw std::invalid_argument("Неподходящий тип памяти!");
 }
 
 void Laptop::setMotherboard(Motherboard motherboard) {
-  this->motherboard = motherboard;
+    this->motherboard = motherboard;
 
-  if (this->cpu.getSocket() != motherboard.getSocket()) { this->cpu = CPU(); }
-  if (this->ram.getRAMType() != motherboard.getSupportedRAMType()) { this->ram = RAM(motherboard.getSupportedRAMType()); }
+    if (this->cpu.getSocket() != motherboard.getSocket()) { this->cpu = CPU(); }
+    if (this->ram.getRAMType() != motherboard.getSupportedRAMType()) { this->ram = RAM(motherboard.getSupportedRAMType()); }
 }
 
 void Laptop::setDisplay(Display display) {
-  this->display = display;
+    this->display = display;
 }
 
 void Laptop::setDataStorage(DataStorage* dataStorage) {
-  DataStorage* newDataStorage = dataStorage;
-  this->dataStorage = newDataStorage;
+    DataStorage* newDataStorage = dataStorage;
+    this->dataStorage = newDataStorage;
 }
 
 void Laptop::input() {
-  std::cout << "Введите название ноутбука: ";
-  std::getline(std::cin, modelName);
-  std::cout << "\tВвод параметров процессора\n";
-  cpu.input();
-  std::cout << "\tВвод параметров видеокарты\n";
-  gpu.input();
-  std::cout << "\tВвод параметров RAM\n";
-  ram.input();
-  std::cout << "\tВвод параметров материнской платы\n";
-  motherboard.input();
-  std::cout << "\tВвод параметров экрана\n";
-  display.input();
-  std::cout << "\tВвод параметров хранилища\n";
-  std::cout << "Введите тип хранилища (0 - HDD, 1 - SSD): ";
-  int choice;
-  do
-  {
-    std::cin >> choice;
-    if (choice != 0 && choice != 1) std::cout << "Некорректный ввод!\nПопробуйте еще раз: ";
-  } while (choice != 0 && choice != 1);
-  dataStorage = createDataStorage(choice);
-  dataStorage->input();
+    std::cout << "Введите название ноутбука: ";
+    std::getline(std::cin, modelName);
+    std::cout << "\tВвод параметров процессора\n";
+    cpu.input();
+    std::cout << "\tВвод параметров видеокарты\n";
+    gpu.input();
+    std::cout << "\tВвод параметров RAM\n";
+    ram.input();
+    std::cout << "\tВвод параметров материнской платы\n";
+    motherboard.input();
+    std::cout << "\tВвод параметров экрана\n";
+    display.input();
+    std::cout << "\tВвод параметров хранилища\n";
+    std::cout << "Введите тип хранилища (0 - HDD, 1 - SSD): ";
+    int choice;
+    do
+    {
+          std::cin >> choice;
+          if (choice != 0 && choice != 1) std::cout << "Некорректный ввод!\nПопробуйте еще раз: ";
+    } while (choice != 0 && choice != 1);
+    dataStorage = createDataStorage(choice);
+    dataStorage->input();
 }
 
 std::string Laptop::toString() const {
   std::string name =
-    "               : " + this->modelName + "\n" +
+    "Название модели: " + this->modelName + "\n" +
     "CPU: " + this->cpu.toString() + "\n" +
     "GPU: " + this->gpu.toString() + "\n" +
     "RAM: " + this->ram.toString() + "\n" +
-    "                 : " + this->motherboard.toString() + "\n" +
-    "     : " + this->display.toString() + "\n" +
+    "Материнская плата: " + this->motherboard.toString() + "\n" +
+    "Экран: " + this->display.toString() + "\n" +
     this->dataStorage->getComponentName() + ": " + dataStorage->toString() + "\n";
   return name;
 }
@@ -128,7 +127,6 @@ bool Laptop::checkArguments(std::string modelName, CPU cpu, GPU gpu, RAM ram, Mo
   return (cpu.getSocket() == motherboard.getSocket()) && (ram.getRAMType() == motherboard.getSupportedRAMType());
 }
 
-Victor, [11/11/2023 17:07]
 void Laptop::tryToSetArguments(std::string modelName, CPU cpu, GPU gpu, RAM ram, Motherboard motherboard, Display display, DataStorage* dataStorage) {
   if (checkArguments(modelName, cpu, gpu, ram, motherboard, display, dataStorage)) {
     this->modelName = modelName;
